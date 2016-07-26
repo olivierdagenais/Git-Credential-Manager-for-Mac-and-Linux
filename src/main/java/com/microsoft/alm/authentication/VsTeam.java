a102dd57978475de3c7e121c5ea1778e936c0690
Oli Dagenais <olivida@microsoft.com>
2016-07-25 20:13:15 -0400

    Add VsTeam[Test] with looksLikeTfsGitPath()

---
+++ src/main/java/com/microsoft/alm/authentication/VsTeam.java
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.authentication;
+
+import com.microsoft.alm.helpers.HttpClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+public class VsTeam
+{
+    public static boolean looksLikeTfsGitPath(final String uriPath)
+    {
+        final String[] parts = uriPath.split("/");
+        boolean result = false;
+        for (int i = 0; i < parts.length; i++)
+        {
+            final String part = parts[i];
+            if (part.equalsIgnoreCase("_git"))
+            {
+                if (i == parts.length - 2)
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/groovy/com/microsoft/alm/authentication/VsTeamTest.groovy
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.authentication
+
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+/**
+ * A class to test {@link VsTeam}.
+ */
+@CompileStatic
+public class VsTeamTest {
+
+    @Test public void looksLikeTfsGitPath_onPremises() {
+        final input = "/tfs/DefaultCollection/Default/_git/CoolBeans";
+
+        final actual = VsTeam.looksLikeTfsGitPath(input);
+
+        assert actual
+    }
+
+    @Test public void looksLikeTfsGitPath_hosted() {
+        final input = "/Default/_git/CoolBeans";
+
+        final actual = VsTeam.looksLikeTfsGitPath(input);
+
+        assert actual
+    }
+
+    @Test public void looksLikeTfsGitPath_gitHub() {
+        final input = "/Microsoft/Git-Credential-Manager-for-Mac-and-Linux.git";
+
+        final actual = VsTeam.looksLikeTfsGitPath(input);
+
+        assert !actual
+    }
+
+}
