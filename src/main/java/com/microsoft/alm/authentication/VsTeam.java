66a0cc2b9152825c74603cd9fccda4356626a592
Oli Dagenais <olivida@microsoft.com>
2016-07-27 15:55:21 -0400

    VsTeam now has super-powers

--- src/main/java/com/microsoft/alm/authentication/VsTeam.java
+++ src/main/java/com/microsoft/alm/authentication/VsTeam.java
@@ -3,7 +3,6 @@
 
 package com.microsoft.alm.authentication;
 
-import com.microsoft.alm.helpers.Action;
 import com.microsoft.alm.helpers.HttpClient;
 import com.microsoft.alm.helpers.IOHelper;
 import com.microsoft.alm.secret.Credential;
@@ -14,11 +13,54 @@ import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 public class VsTeam
 {
     private static final String X_TFS_PROCESS_ID = "X-TFS-ProcessId";
     private static final String X_VSS_RESOURCE_TENANT = "X-VSS-ResourceTenant";
+
+    private final URI repoUri;
+    private final HttpClient client;
+    private final Map<String, List<String>> headerFields;
+
+    public VsTeam(final URI repoUri)
+    {
+        this.repoUri = repoUri;
+        this.client = new HttpClient(Global.getUserAgent());
+        try
+        {
+            final HttpURLConnection response = client.head(repoUri);
+            this.headerFields = response.getHeaderFields();
+        }
+        catch (final IOException e)
+        {
+            throw new Error(e);
+        }
+    }
+
+    public boolean isTeamFoundationServer()
+    {
+        final String repoUriPath = repoUri.getPath();
+        if (!looksLikeTfsGitPath(repoUriPath))
+        {
+            return false;
+        }
+        final List<String> tfsProcessIds = headerFields.get(X_TFS_PROCESS_ID);
+        if (hasValue(tfsProcessIds))
+        {
+            // it could still be Team Services; if so, it will have the X-VSS-ResourceTenant header
+            final List<String> resourceTenants = headerFields.get(X_VSS_RESOURCE_TENANT);
+            return !hasValue(resourceTenants);
+        }
+        return false;
+    }
+
+    static boolean hasValue(final List<String> headerValues)
+    {
+        return headerValues != null && headerValues.size() > 0;
+    }
 
     public static boolean looksLikeTfsGitPath(final String uriPath)
     {
@@ -39,24 +81,10 @@ public class VsTeam
         return result;
     }
 
-    public static boolean isTeamFoundationServer(final URI repoUri) throws IOException
+    public static boolean isTeamFoundationServer(final URI repoUri)
     {
-        final String repoUriPath = repoUri.getPath();
-        if (!looksLikeTfsGitPath(repoUriPath))
-        {
-            return false;
-        }
-
-        final HttpClient client = new HttpClient(Global.getUserAgent());
-        final HttpURLConnection response = client.head(repoUri);
-        final String tfsProcessId = response.getHeaderField(X_TFS_PROCESS_ID);
-        if (tfsProcessId != null)
-        {
-            // it could still be Team Services; if so, it will have the X-VSS-ResourceTenant header
-            final String resourceTenant = response.getHeaderField(X_VSS_RESOURCE_TENANT);
-            return resourceTenant == null;
-        }
-        return false;
+        final VsTeam vsTeam = new VsTeam(repoUri);
+        return vsTeam.isTeamFoundationServer();
     }
 
     public static class PasswordAuthenticator extends Authenticator implements Closeable
